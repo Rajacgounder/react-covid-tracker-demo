edfce13701cd3e6fce62a689df6e6cdcb86d69a9
Raja_C <33963529+Rajacgounder@users.noreply.github.com>
2021-06-03 12:32:43 +0530

    Cchange in Image floder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import { Cards, Chart, CountryPicker } from './components';
-import styles from './App.module.css';
-import { fetchData } from './api';
-import coronaImage from './images/coronavirus.png';
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 
+import Image from "./images/coronavirus.png";
 class App extends React.Component {
   state = {
     data: {},
-    country: ''
+    country: ""
   };
 
   async componentDidMount() {
@@ -17,7 +17,7 @@ class App extends React.Component {
     this.setState({ data: fetchedData });
   }
 
-  handleCountryChange = async country => {
+  handleCountryChange = async (country) => {
     const fetchedData = await fetchData(country);
 
     this.setState({ data: fetchedData, country: country });
@@ -27,7 +27,7 @@ class App extends React.Component {
     const { data, country } = this.state;
     return (
       <div className={styles.container}>
-        <img className={styles.image} src={coronaImage} alt="Covid !9" />
+        <img className={styles.image} src={Image} alt="Covid !9" />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Cards data={data} />
 
